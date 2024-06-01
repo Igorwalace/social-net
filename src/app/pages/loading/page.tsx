121b645c4395ebf34925cc88eba5e4d5d1afdcbe
igoor <igorwallacy3@gmail.com>
2024-06-01 15:05:32 -0300

    LoadingProps

--- src/app/pages/loading/page.tsx
+++ src/app/pages/loading/page.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
-interface loading {
+interface LoadingProps {
     loading: boolean
 }
 
-const Loading = ({ loading }: loading) => {
+const Loading = ({ loading }: LoadingProps) => {
     return (
-        <>{
-            loading &&
-            <main className='fixed -top-10 bottom-0 right-0 left-0 flex items-center justify-center z-50 bg-[rgb(0,0,0,0.3)]' >
-                <span className="loader"></span>
-            </main>
-        }
+        <>
+            {
+                loading &&
+                <main className='fixed -top-10 bottom-0 right-0 left-0 flex items-center justify-center z-50 bg-[rgb(0,0,0,0.3)]' >
+                    <span className="loader"></span>
+                </main>
+            }
         </>
     )
 }
